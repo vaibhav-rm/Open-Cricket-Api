f3c3ff405dc7972d45030ea7e706c07881f98557
vaibhav-rm <rathodvaibhav401@gmail.com>
2025-01-19 22:16:11 +0530

    Fresh spring boot project

---
+++ src/test/java/com/cricketstats/api/cricket_api/CricketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cricketstats.api.cricket_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
